f2d926f6f2bb6d19ff09a1096d3d36acfd6ad98a
AustinkChow <austinkchow@gmail.com>
2020-07-17 15:36:41 -0700

    install mongolab

--- rubber-duck-interviewer/src/components/LandingPageQuestions/LandingPageQuestions.js
+++ rubber-duck-interviewer/src/components/LandingPageQuestions/LandingPageQuestions.js
@@ -21,18 +21,20 @@ class QuestionContainer extends Component {
         console.log(this.props)
         return (
             <>
-                <div className='question_number'>
-                    {this.props.questions.length > 0 &&
-                        <h3>{this.state.currentIndex + 1}/{this.props.questions.length}</h3>
-                    }
-                </div>
-                <div className='question_container'>
-                    <button onClick={this.previous}>Prev</button>
-                    <div className='display_question'>
-                        <h2>{this.props.questions[this.state.currentIndex]}</h2>
-                    </div>
-                    <button onClick={this.next}>Next</button>
-                </div>
+                {this.props.questions.length > 0 &&
+                    <>
+                        <div className='question_number'>
+                            <h3>{this.state.currentIndex + 1}/{this.props.questions.length}</h3>
+                        </div>
+                        <div className='question_container'>
+                            <button onClick={this.previous}>Prev</button>
+                            <div className='display_question'>
+                                <h2>{this.props.questions[this.state.currentIndex]}</h2>
+                            </div>
+                            <button onClick={this.next}>Next</button>
+                        </div>
+                    </>
+                }
             </>
         )
     }
